860367a6db0124fc0e83c9f90c19c0591430a4ea
Jacek Achtelik <jacekachtelik@gmail.com>
2017-12-07 07:22:22 +0100

    mod02task06 v3

--- html/mod03/mod03task05/script.js
+++ html/mod03/mod03task05/script.js
@@ -1,10 +1,10 @@
-const onTimeout = () => console.log('Zapisano!');
-
-const onWriteSuccess = () => setTimeout(onTimeout, 1000);
-
-const onReadSuccess(error, text) => {
-  console.log(text);
-  fs.writeFile('myfile-copy.txt', text, onWriteSuccess);
-}
-
-fs.readFile('myfile.txt', { encoding: 'utf8' }, onReadSuccess);
+//const onTimeout = () => console.log('Zapisano!');
+//
+//const onWriteSuccess = () => setTimeout(onTimeout, 1000);
+//
+//const onReadSuccess(error, text) => {
+//  console.log(text);
+//  fs.writeFile('myfile-copy.txt', text, onWriteSuccess);
+//}
+//
+//fs.readFile('myfile.txt', { encoding: 'utf8' }, onReadSuccess);
