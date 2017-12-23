5a5bca6e0c0f97264aaed16845268e616455cf3b
Jacek Achtelik <jacekachtelik@gmail.com>
2017-12-23 16:43:30 +0100

    m04t04 - Rozwój aplikacji ToDo. Problem tylko z przyciskiem usuń

--- html/mod04/mod04task04/webpack.config.js
+++ html/mod04/mod04task04/webpack.config.js
@@ -1,11 +1,39 @@
 const path = require('path');
+// var environment = 'production';
+var environment = 'development';
+
+var env = process.env.NODE_ENV || environment;
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var OptimizeJsPlugin = require('optimize-js-plugin');
+
+
+var plugins = [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+}
+
 //webpack.config.js
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.js'
+        ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
@@ -32,13 +60,5 @@ module.exports = {
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: 'src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    }),
-        new webpack.optimize.UglifyJsPlugin(),
-        new OptimizeJsPlugin({
-            sourceMap: false
-        })]
+    plugins: plugins
 };
