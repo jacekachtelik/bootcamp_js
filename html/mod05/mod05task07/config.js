a997c1720ecfb72fea28d8c9f4c393576ee1343f
Jacek Achtelik <jacekachtelik@gmail.com>
2017-12-23 21:37:53 +0100

    m05t07 Logowanie Google

---
+++ html/mod05/mod05task07/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'654753246257-01vbfp3dq8juj52nfjpbkfcgpurjbom4.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET':'gc_EI1IflKZBEyfh8JWZ2DgJ',
+    'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
