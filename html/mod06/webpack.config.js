816f81073f2afbf45f952ffcfaf972c895fe4f20
Jacek Achtelik <jacekachtelik@gmail.com>
2017-12-27 21:41:44 +0100

    m06t01 Chat - wersja nie działająca

---
+++ html/mod06/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+// var environment = 'production';
+var environment = 'development';
+
+var env = process.env.NODE_ENV || environment;
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+var plugins = [
+    new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+}
+
+//webpack.config.js
+module.exports = {
+    entry: (env !== 'production' ? [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: plugins
+};
