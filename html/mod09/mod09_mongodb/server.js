08bc56eacd13e03ac77aa16706e20b9d6cf3ba17
Jacek Achtelik <jacekachtelik@gmail.com>
2018-01-02 21:26:12 +0100

    m09t08 Heroku - poprawka

--- html/mod09/mod09_mongodb/server.js
+++ html/mod09/mod09_mongodb/server.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+// mongoose.connect('mongodb://localhost/nodeappdatabase', {
+//     useMongoClient: true
+// });
+
+mongoose.connect('mongodb://mongodb_user_ja:123qwe@ds235827.mlab.com:35827/users' {
     useMongoClient: true
 });
 
